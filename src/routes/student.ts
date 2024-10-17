fe98026948e81762674cb4e6aff707aaac31d5f0
YaelChar417 <A01711111@tec.mx>
2024-10-17 17:48:28 -0600

    Hola este es mi 7mo commit

--- src/routes/student.ts
+++ src/routes/student.ts
@@ -1,9 +1,18 @@
 // routes/usuarios.ts
 import { Router } from "express";
-import { getStudents } from "../controllers/student";
+import {
+  createStudent,
+  deleteStudent,
+  getStudents,
+  updateStudent,
+} from "../controllers/student";
+//import { create } from "domain";
 
 const router = Router();
 
 router.get("/students", getStudents);
+router.post("/", createStudent);
+router.post("/:id", updateStudent);
+router.post("/id:", deleteStudent);
 
 export default router;
--- src/services/student.ts
+++ src/services/student.ts
@@ -1,6 +1,20 @@
-import { findAllStudents } from "../models/student";
+import { deleteStudent, updateStudent } from "../models/student";
+import { Student } from "../interfaces/student";
+import { findAllStudents, insertStudent } from "../models/student";
 
 // Obtener todos los alumnos
 export const findAll = async () => {
   return await findAllStudents();
+};
+
+export const insert = async (student: Student) => {
+  return await insertStudent(student);
+};
+
+export const update = async (id: number, student: Student) => {
+  return await updateStudent(id, student);
+};
+
+export const deleteById = async (id: number) => {
+  return await deleteStudent(id);
 };
