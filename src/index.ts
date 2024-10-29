8f63ba47f1f5b7a0c775ec54371a55051941b303
YaelChar417 <A01711111@tec.mx>
2024-10-29 17:56:01 -0600

    Feature: Hola no se en que commit voy HOLA PROFE

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 import express from "express";
 import studentRouter from "./routes/student";
+import unknownResource from "./middlewares/unknown-resources";
 
 //Para poder acceder a las variables del ambiente (.env)
 config();
@@ -10,8 +11,12 @@ const app = express();
 // Middleware para parsear JSON
 app.use(express.json());
 
+//rutas de la api
 app.use("/student", studentRouter);
 
 app.listen(process.env.SERVER_PORT, function () {
   console.log("Escuchando puerto " + process.env.SERVER_PORT);
 });
+
+// Middlewares
+app.use(unknownResource);
